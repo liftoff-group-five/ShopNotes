48612c0ef64894a980886e4e001bd77cbd087e59
Xoadra <xijp77@gmail.com>
2018-02-08 21:39:53 -0800

    Meta app moved to root and Angular working on Heroku.

--- cupcakes/settings.py
+++ cupcakes/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-	'apps.meta',
+	'meta',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,17 +127,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname( os.path.abspath( __file__ ) ) 
-#STATIC_ROOT = os.path.join( BASE_DIR, '/cupcakes/static/' )
+PROJECT_ROOT = os.path.dirname( os.path.abspath( __file__ ) )
 STATIC_ROOT = os.path.join( PROJECT_ROOT, 'staticfiles' )
 STATIC_URL = '/static/'
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
 	os.path.join( PROJECT_ROOT, 'static' ),
-	#os.path.join( BASE_DIR, 'static' )
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
--- cupcakes/urls.py
+++ cupcakes/urls.py
@@ -23,7 +23,7 @@ from django.urls import path, include
 
 urlpatterns = [
 	path( 'admin/', admin.site.urls ),
-	path( '', include( 'apps.meta.urls' ) ),
+	path( '', include( 'meta.urls' ) ),
 ]
 
 
